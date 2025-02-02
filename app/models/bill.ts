5b3d16c4a4eff59313916fdc176287c0c2538104
Kozell Yon-Brooks <ko3lyonbrooks@gmail.com>
2025-02-02 20:53:31 +0000

    add comments to models

--- app/models/bill.ts
+++ app/models/bill.ts
@@ -1,32 +1,29 @@
 // Define a type for a bill
 export type Bill = {
     id: string;
-    total: number;
+    total: number; // This should be a user entered total
+    subTotal: number; // This should be calculated from the sum of the bill items totals
     items: BillItem[];
 };
 
-// Define a type for a bill item
 export type BillItem = {
     id: string;
     name: string;
-    price: number;
+    price: number; // if quantity and total price is entered, then price is totalPrice / quantity
     quantity: number;
-    totalPrice: number;
+    totalPrice: number; // if quantity and price is entered, then totalPrice is price * quantity
     assignedTo: string[]; // Array of participant IDs
 };
 
-// Define a type for a participant
 export type Payee = {
     id: string;
     name: string;
-    partySize: number;
+    partySize: number; // Accounts for service charge split when paying for multiple people.
 };
 
-
-// Define a type for a participant
 export type DiscountItem = {
     id: string;
     usePercentage: boolean;
-    percentage: number;
+    percentage: number; // either percentage or amount is used, not both
     amount: number;
 };
